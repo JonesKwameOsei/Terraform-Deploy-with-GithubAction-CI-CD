7e108c1167592513112230f3c33cc993f2d7fa5d
Jones Kwame Osei <oseikwamejones@gmail.com>
2024-07-04 16:44:26 +0100

    Destroy deployment

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,5 @@
-module "ecr" {
-    source = "../modules/tf-ecr"
-    ecr_name = "jones" # this is ecr name 
-}
+# module "ecr" {
+#     source = "../modules/tf-ecr"
+#     ecr_name = "jones" # this is ecr name 
+# }
 
--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,26 +1,26 @@
-#------------------------------------------------------------------------------
-# ECS Configurations
-#------------------------------------------------------------------------------
-module "ecs" {
-  source              = "../modules/tf-ecs"
-  name                = var.name
-  account_id          = var.account_id # needs to be removed
-  account_name        = var.account_name
-  aws_region          = var.region
-  vpc_id              = data.aws_ssm_parameter.vpc_id.value
-  vpc_ip_cidr         = data.aws_ssm_parameter.vpc_cidr.value
-  private_subnet_ids  = local.private_subnet_ids
-  public_subnets_ids  = local.public_subnet_ids
-  ssl_certificate_arn = data.aws_ssm_parameter.certificate_arn.value
-  container_name      = var.name
-  image               = "chriscloudaz/netflix:latest" #"194626909496.dkr.ecr.eu-west-1.amazonaws.com/jones"
-  app_port            = 80
-  app_count           = 2
-  container_memory    = 2048
-  container_cpu       = 1024
-  zone_id             = var.zone_id
-  dns_record_name     = "ecs"  #updated
-  health_check_path   = "/"
-}
+# #------------------------------------------------------------------------------
+# # ECS Configurations
+# #------------------------------------------------------------------------------
+# module "ecs" {
+#   source              = "../modules/tf-ecs"
+#   name                = var.name
+#   account_id          = var.account_id # needs to be removed
+#   account_name        = var.account_name
+#   aws_region          = var.region
+#   vpc_id              = data.aws_ssm_parameter.vpc_id.value
+#   vpc_ip_cidr         = data.aws_ssm_parameter.vpc_cidr.value
+#   private_subnet_ids  = local.private_subnet_ids
+#   public_subnets_ids  = local.public_subnet_ids
+#   ssl_certificate_arn = data.aws_ssm_parameter.certificate_arn.value
+#   container_name      = var.name
+#   image               = "chriscloudaz/netflix:latest" #"194626909496.dkr.ecr.eu-west-1.amazonaws.com/jones"
+#   app_port            = 80
+#   app_count           = 2
+#   container_memory    = 2048
+#   container_cpu       = 1024
+#   zone_id             = var.zone_id
+#   dns_record_name     = "ecs"  #updated
+#   health_check_path   = "/"
+# }
 
-#--------------------------------------------------------
+# #--------------------------------------------------------
